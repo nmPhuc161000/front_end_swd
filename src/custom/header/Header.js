2cae70b9a839d4c70ae0bb4fa53b3bc4bcc0b4e7
loi2003 <truongthangloi2003@gmail.com>
2024-10-08 22:27:58 +0700

    header

--- src/custom/header/Header.js
+++ src/custom/header/Header.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  )
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className='NavbarItems'>
+        <h1 className='navbar-logo'>
+          <i className='material-icons'>local_florist</i> React
+        </h1>
+        <ul className='nav-menu'>
+          <li>
+            <Link to='/' className='nav-links'>
+              <i className='material-icons'>home</i> Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/services' className='nav-links'>
+              <i className='material-icons'>build</i> Services
+            </Link>
+          </li>
+          <li>
+            <Link to='/products' className='nav-links'>
+              <i className='material-icons'>storefront</i> Products
+            </Link>
+          </li>
+          <li>
+            <Link to='/sign-up' className='nav-links'>
+              <i className='material-icons'>person_add</i> Sign Up
+            </Link>
+          </li>
+        </ul>
+        <div className='search-bar'>
+          <input type='text' placeholder='Search...' />
+          <button className='search-button'>
+            <i className='material-icons'>search</i>
+          </button>
+        </div>
+      </nav>
+    );
+  }
 }
+
+export default Navbar;
