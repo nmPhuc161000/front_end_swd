82a3ed17176d98d52ea743be0a22f0af027191d6
nmPhuc161000 <phucnmse161000@fpt.edu.vn>
2024-10-25 14:21:03 +0700

    add profile sshop

--- src/components/profiles/Shop/Shop.js
+++ src/components/profiles/Shop/Shop.js
@@ -3,13 +3,14 @@ import "./Shop.css";
 import ShopNavBar from "./shop-navbar/ShopNavBar";
 import { Route, Routes } from "react-router-dom";
 import ServicesHistory from "./serviceHistory/ServicesHistory";
+import ShopService from "./shop-services/ShopService";
 
 export default function Shop() {
   return (
     <div className="shop">
       <ShopNavBar />
       <Routes>
-        <Route path="shop" />
+        <Route path="shop" element={<ShopService />} />
         <Route path="mylog/:id"></Route>
         <Route path="servicesHistory" element={<ServicesHistory />}></Route>
       </Routes>
